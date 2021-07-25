b651908a8513ee04d74d207d77a66885a355b3d5
mribr <mr.ibrahim@hotmail.se>
2021-07-25 16:53:34 +0200

    9nd Section Two, Video Two, new activity created

--- app/src/main/java/com/example/msgshareapp_git/MainActivity.kt
+++ app/src/main/java/com/example/msgshareapp_git/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.msgshareapp_git
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -21,9 +21,9 @@ class MainActivity : AppCompatActivity() {
         btnSendMsgToNextActivity.setOnClickListener{
 
             val message: String = etUserMessage.text.toString()
-            Toast.makeText(this,message,Toast.LENGTH_LONG).show()
-
-
+            Toast.makeText(this,"Welcome "+ message,Toast.LENGTH_LONG).show()
+            val intent = Intent(this, Activity2::class.java)
+            startActivity(intent)
         }
     }
 }
