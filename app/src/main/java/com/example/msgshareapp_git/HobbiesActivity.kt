c4492670cbc23d2f12d2c20788016ee439f53f39
mribr <mr.ibrahim@hotmail.se>
2021-07-26 02:15:05 +0200

    15nd Section Three, Video five, HobbiesAdapter.kt was added modified

--- app/src/main/java/com/example/msgshareapp_git/HobbiesActivity.kt
+++ app/src/main/java/com/example/msgshareapp_git/HobbiesActivity.kt
@@ -2,10 +2,25 @@ package com.example.msgshareapp_git
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_hobbies.*
 
 class HobbiesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hobbies)
+        //Step1
+        val layoutManager =LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+
+        //Step2
+        val adapter = HobbiesAdapter(this, Supplier.hobbies)
+        recyclerView.adapter = adapter
+
+        //Step3 - attach layoutManger to recyclerview
+        recyclerView.layoutManager = layoutManager
+
+
+
     }
 }
